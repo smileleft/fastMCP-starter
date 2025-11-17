7c98d63f0607e71386c0d02f947e8b05c876bb4a
smileleft <spyt77@gmail.com>
2025-11-17 14:19:10 +0900

    Update first_server.py

--- first_server.py
+++ first_server.py
@@ -4,9 +4,19 @@ import asyncio
 
 APP_CONFIG = {"theme": "dark", "version": "1.1", "feature_flags": ["new_dashboard"]}
 
+USER_PROFILES = {
+    101: {"name": "Alice", "status": "active"},
+    102: {"name": "Bob", "status": "inactive"},
+}
+
 mcp = FastMCP(name="my first mcp server")
 
 print("First FastMCP server is created.")
+
+@mcp.resource("users://{user_id}/profile")
+def get_user_profile(user_id: int) -> dict:
+    """Search User Profile with User ID."""
+    return USER_PROFILES.get(user_id, {"error": "cannot find user."})
 
 @mcp.resource("data://config")
 def get_config() -> dict:
