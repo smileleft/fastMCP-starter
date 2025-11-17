dca51c9ae29c9bc5633011333b80dc3ea46b6d11
smileleft <spyt77@gmail.com>
2025-11-17 14:14:06 +0900

    Update first_server.py

--- first_server.py
+++ first_server.py
@@ -5,3 +5,16 @@ import asyncio
 mcp = FastMCP(name="my first mcp server")
 
 print("First FastMCP server is created.")
+
+@mcp.tool()
+def greet(name: str) -> str:
+  """simple greeting"""
+  return f"Hello, {name}!"
+
+@mcp.tool()
+def add(a: int, b: init) -> int:
+  """add two numbers."""
+  return a + b
+
+print("tool 'greet' and 'add' is added.")
+  
